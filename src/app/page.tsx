f23d67dae87a0403a076dcd257743704b34a8062
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2024-12-03 14:50:22 +0100

    user authentication logout and login screens and redirecting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { useCurrent } from "./features/auth/api/use-current";
+import { useLogout } from "./features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div>
-      <Button variant="primary" className="xs:display-block md:display-none ">
-        Primary
-      </Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="teritary">Teritary</Button>
-      <Input />
+      Only visible to autorize users
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
