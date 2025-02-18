2c609b1e979ef0b5b647a34b31167ad4106a2463
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-18 16:14:58 +0100

    fix build error async func sign in

--- src/app/(auth)/sign-up/SignUpPage.tsx
+++ src/app/(auth)/sign-up/SignUpPage.tsx
@@ -1,6 +1,8 @@
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
 
-export const SignUpPage = () => {
+export const SignUpPage = async () => {
   const user = await getCurrent();
 
   if (user) redirect("/");
