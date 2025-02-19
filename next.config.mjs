75b2151a5e975258dd355dc22e694adcf769a516
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-19 10:19:19 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
 export default nextConfig;
