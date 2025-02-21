358580cb82f9fd37344b3b7a93a20325d3905af7
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-21 12:28:02 +0100

    implementation of invite link logic

---
+++ src/features/workspaces/api/use-join-workspace.ts
@@ -0,0 +1,44 @@
+import { toast } from "sonner";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.workspaces)[":workspaceId"]["join"]["$post"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[":workspaceId"]["join"]["$post"]
+>;
+
+export const useJoinWorkspace = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param, json }) => {
+      const respone = await client.api.workspaces[":workspaceId"]["join"][
+        "$post"
+      ]({
+        param,
+        json,
+      });
+
+      if (!respone.ok) {
+        throw new Error("Failed to join workspace");
+      }
+
+      return await respone.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Joined workspace");
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      queryClient.invalidateQueries({ queryKey: ["workspace", data.$id] });
+    },
+    onError: () => {
+      toast.error("Failed to join workspace");
+    },
+  });
+  return mutation;
+};
