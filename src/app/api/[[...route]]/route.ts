fbff643d43efa3d03641ae50f37af8a88ec4dfbd
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-24 16:12:25 +0100

    ui connected with backend and appwrite for member role manipulation

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import auth from "@/features/auth/server/route";
-import members from "@/features/auth/server/route";
+import members from "@/features/members/server/route";
 import workspaces from "@/features/workspaces/server/route";
 
 const app = new Hono().basePath("/api");
