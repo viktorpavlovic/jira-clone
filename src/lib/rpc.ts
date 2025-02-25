76f12c0433c27b1c3d68b434909058634268d67d
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-25 14:36:36 +0100

    fixing env variable for vercel deploy

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,4 +1,7 @@
 import { hc } from "hono/client";
 import { AppType } from "@/app/api/[[...route]]/route";
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+export const client = hc<AppType>(
+  process.env.NEXT_PUBLIC_APP_URL ||
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+);
