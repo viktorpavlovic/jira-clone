ff84e059369cc1102a0fe63372d2b84b7e0b7da5
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-27 17:11:01 +0100

    creating get route for tasks

--- src/features/tasks/schemas.ts
+++ src/features/tasks/schemas.ts
@@ -8,6 +8,6 @@ export const createTaskSchema = z.object({
   workspaceId: z.string().trim().min(1, "Required"),
   projectId: z.string().trim().min(1, "Required"),
   dueDate: z.coerce.date(),
-  assigneId: z.string().trim().min(1, "Required"),
+  assigneeId: z.string().trim().min(1, "Required"),
   description: z.string().optional(),
 });
