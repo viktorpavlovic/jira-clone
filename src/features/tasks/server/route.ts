90c2adc1c2bc1d42391ac6cb231683f2d644aab9
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-27 16:46:56 +0100

    adding post method route

---
+++ src/features/tasks/server/route.ts
@@ -0,0 +1,69 @@
+import { Hono } from "hono";
+import { ID, Query } from "node-appwrite";
+import { zValidator } from "@hono/zod-validator";
+
+import { getMember } from "@/features/members/utils";
+
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { DATABASE_ID, TASKS_ID } from "@/config";
+
+import { createTaskSchema } from "../schemas";
+
+getMember;
+
+const app = new Hono().post(
+  "/",
+  sessionMiddleware,
+  zValidator("json", createTaskSchema),
+  async (c) => {
+    const user = c.get("user");
+    const databases = c.get("databases");
+    const { name, status, workspaceId, projectId, dueDate, assigneId } =
+      c.req.valid("json");
+
+    const member = await getMember({
+      databases,
+      workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member) {
+      return c.json({ error: "Unauthorized" }, 400);
+    }
+
+    const highestPositionTask = await databases.listDocuments(
+      DATABASE_ID,
+      TASKS_ID,
+      [
+        Query.equal("status", status),
+        Query.equal("workspaceId", workspaceId),
+        Query.orderAsc("position"),
+        Query.limit(1),
+      ]
+    );
+
+    const newPosition =
+      highestPositionTask.documents.length > 0
+        ? highestPositionTask.documents[0].position + 1000
+        : 1000;
+
+    const task = await databases.createDocument(
+      DATABASE_ID,
+      TASKS_ID,
+      ID.unique(),
+      {
+        name,
+        status,
+        workspaceId,
+        projectId,
+        dueDate,
+        assigneId,
+        position: newPosition,
+      }
+    );
+
+    return c.json({ data: task });
+  }
+);
+
+export default app;
