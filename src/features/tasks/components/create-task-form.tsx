2dfa7cd055c61f680fb43732d89b18e448e9de9c
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-02-28 13:36:12 +0100

    fix build

--- src/features/tasks/components/create-task-form.tsx
+++ src/features/tasks/components/create-task-form.tsx
@@ -47,8 +47,6 @@ export const CreateTaskForm = ({
 }: CreateTaskFormProps) => {
   const workspaceId = useWorkspaceId();
 
-  const router = useRouter();
-
   const { mutate, isPending } = useCreateTask();
 
   const form = useForm<z.infer<typeof createTaskSchema>>({
