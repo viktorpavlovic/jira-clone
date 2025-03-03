052bfe5a9a56828f86c14d42c02793e049c41fa5
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2025-03-03 10:26:12 +0100

    fix

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -2,5 +2,6 @@ import { hc } from "hono/client";
 import { AppType } from "@/app/api/[[...route]]/route";
 
 export const client = hc<AppType>(
-  process.env.NEXT_PUBLIC_APP_URL || process.env.DOMAIN_URL!
+  process.env.NEXT_PUBLIC_APP_URL ||
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 );
